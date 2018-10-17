ec2c29c56a4d9923d1a0d7372c51acca6426687f
annika <annikaslund@gmail.com>
2018-10-17 16:42:52 -0700

    Complete 04a

--- skeleton 3/lib/04a_countries_bonus.rb
+++ skeleton 3/lib/04a_countries_bonus.rb
@@ -17,6 +17,20 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    countries
+  WHERE
+    gdp > ALL(
+      SELECT
+        gdp
+      FROM
+        countries
+      WHERE
+        continent = 'Europe' AND
+          gdp IS NOT NULL
+    )
   SQL
 end
 
@@ -24,6 +38,20 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+  SELECT
+    x.continent, x.name, x.area
+  FROM
+    countries AS x
+  WHERE
+    x.area = (
+      SELECT
+        MAX(y.area)
+      FROM
+        countries AS y
+      WHERE
+        x.continent = y.continent
+    )
+
   SQL
 end
 
@@ -31,5 +59,20 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+  SELECT
+    x.name, x.continent
+  FROM
+    countries AS x
+  WHERE
+    x.population/3 > ALL(
+      SELECT
+        y.population
+      FROM
+        countries AS y
+      WHERE
+        x.continent = y.continent
+      AND
+        x.name != y.name 
+    )
   SQL
 end
